7411ef80f6838fb91b51dfb94bb4e7d7d4f4c982
Uncensored-Developer <osezele.o@gmail.com>
2020-08-01 07:56:13 +0000

    add Contact model

--- eandel/users/models.py
+++ eandel/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db  import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +8,24 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Contact(models.Model):
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=500)
+    msg = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.subject)
+
+    class Meta:
+        ordering = ['-created_at']
+    
