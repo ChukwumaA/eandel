47e4aa2d7c365ee222d8a472421dd4153bc5b06e
Osezele Orukpe <osezele.o@gmail.com>
2021-01-01 21:56:24 +0100

    add Contact model to admin

--- eandel/users/admin.py
+++ eandel/users/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
 from eandel.users.forms import UserChangeForm, UserCreationForm
+from .models import Contact
 
 User = get_user_model()
 
@@ -15,3 +16,9 @@ class UserAdmin(auth_admin.UserAdmin):
     fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'email', 'subject', 'created_at']
