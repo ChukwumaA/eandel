040d8ca4909287ab2360554ea3b7f97014f27764
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2024-10-27 16:42:52 +0100

    Amend: fixed internal server error.

--- config/settings/base.py
+++ config/settings/base.py
@@ -64,9 +64,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "****",
+        "PASSWORD": "WitnessMe23",
         "HOST": "localhost",
-        "PORT": "****",
+        "PORT": "5432",
         "OPTIONS": {
             "options": "-c search_path=public"
         },
@@ -189,10 +189,18 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
 TEMPLATES = [
     {
+        # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
         "BACKEND": "django.template.backends.django.DjangoTemplates",
+        # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
         "DIRS": [str(APPS_DIR / "templates")],
-        "APP_DIRS": True,
         "OPTIONS": {
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
+            # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ],
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
